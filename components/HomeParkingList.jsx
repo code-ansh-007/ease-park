831617ad886d5823e556ccd9d48293562a597d3e
Ansh <anshpradhancareer03@gmail.com>
2023-09-28 10:52:28 +0530

    made the distance logic of for the small card

--- components/HomeParkingList.jsx
+++ components/HomeParkingList.jsx
@@ -3,7 +3,47 @@ import ParkingCard from "./Parkingcard";
 import SmallCard from "./SmallCard";
 
 const vt = VT323({ subsets: ["latin"], weight: ["400"] });
-function HomeParkingList({ sites }) {
+function HomeParkingList({ sites, location }) {
+  // ? DISTANCE CALCULATION FUNCTION
+
+  function calculateDistance(lat1, lon1, lat2, lon2) {
+    const earthRadiusKm = 6371;
+
+    const deg2rad = (deg) => deg * (Math.PI / 180);
+    const lat1Rad = deg2rad(lat1);
+    const lon1Rad = deg2rad(lon1);
+    const lat2Rad = deg2rad(lat2);
+    const lon2Rad = deg2rad(lon2);
+
+    const dLat = lat2Rad - lat1Rad;
+    const dLon = lon2Rad - lon1Rad;
+    const a =
+      Math.sin(dLat / 2) ** 2 +
+      Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.sin(dLon / 2) ** 2;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = earthRadiusKm * c;
+
+    return distance;
+  }
+
+  const updateSitesWithDistances = () => {
+    const updatedSites = sites.map((site) => {
+      const distance = calculateDistance(
+        location.coordinates.lat,
+        location.coordinates.lng,
+        site.location.latitude,
+        site.location.longitude
+      );
+
+      return { ...site, distance };
+    });
+    updatedSites.sort((a, b) => a.distance - b.distance);
+
+    return updatedSites;
+  };
+
+  const updatedSites = updateSitesWithDistances();
+
   return (
     <main className={`bg-[#161B24]  ${vt.className} pt-3`}>
       <div className=" h-full bg-[#1c2331] rounded-t-3xl px-4">
@@ -18,7 +58,7 @@ function HomeParkingList({ sites }) {
         {/* <div className="flex justify-center items-center pt-4 pb-32"></div>
         <div className="pt-24"></div> */}
         <div className="grid grid-cols-2 gap-5">
-          {sites.map((site, index) => (
+          {updatedSites.map((site, index) => (
             <SmallCard key={index} site={site} />
           ))}
         </div>
