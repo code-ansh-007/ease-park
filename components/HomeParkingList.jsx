6211c1369d81770ffc9e5d34008ddd9d4f1c73fe
Dewansh Nehra <dewanshnehra@gmail.com>
2023-09-28 10:23:27 +0530

    minor color change

--- components/HomeParkingList.jsx
+++ components/HomeParkingList.jsx
@@ -6,7 +6,7 @@ function HomeParkingList(props) {
   return (
     <main className={`bg-[#161B24]  ${vt.className} pt-3`}>
       <div className=" h-full bg-[#1c2331] rounded-t-3xl">
-        <div className="flex justify-evenly pt-4">
+        <div className="flex justify-evenly pt-4 text-white">
           <button className="flex flex-col justify-center items-center bg-[#319a3c54]  h-14 w-16 rounded-xl border-2 border-[#319a3c] border-solid">
             {" "}
             <img src="car.svg" alt="" /> Car
